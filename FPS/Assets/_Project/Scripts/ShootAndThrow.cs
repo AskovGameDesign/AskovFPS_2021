36a9c64305047eb535db22be079388dc9b4e8dd1
Claus Bjerre Jacobsen <claus@aneks.dk>
2021-01-25 14:38:06 +0100

    Zombie animations

--- FPS/Assets/_Project/Scripts/ShootAndThrow.cs
+++ FPS/Assets/_Project/Scripts/ShootAndThrow.cs
@@ -9,23 +9,68 @@ public class ShootAndThrow : MonoBehaviour
     public float throwForce = 300f;
     public Transform throwFromHere;
     public int numberOfBabies = 69;
+    public AudioClip throwSound;
+
+    [Header("Projectile")]
+    public float projectileDamage = 5f;
+    public float projectileRange = 200f;
+    public float fireRate = 0.5f;
+    private Camera playerCamera;
+    private float timeSinceLastShot;
+    public AudioClip shootSound;
+
+    private AudioSource audio;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCamera = GetComponentInChildren<Camera>();
+        audio = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && numberOfBabies > 0)
+        if (Input.GetMouseButtonDown(1) && numberOfBabies > 0)
         {
             numberOfBabies--;
             if (numberOfBabies < 0)
                 numberOfBabies = 0;
 
+            if(audio && throwSound)
+            {
+                audio.PlayOneShot(throwSound);
+            }
+
             GameObject baby = Instantiate(throwObject, throwFromHere.position, Quaternion.Euler(Random.value * 360, Random.value * 360,Random.value * 360));
             baby.GetComponent<Rigidbody>().AddForce(throwFromHere.forward * throwForce, ForceMode.Impulse);
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+
+            if (audio && shootSound)
+                audio.PlayOneShot(shootSound);
+        }
+
+        timeSinceLastShot += Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        if(timeSinceLastShot >= fireRate)
+        {
+            Ray ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0f));
+
+            if(Physics.Raycast(ray, out RaycastHit hit, projectileRange))
+            {
+                if(hit.collider.GetComponent<IDamageable>() != null)
+                {
+                    hit.collider.GetComponent<IDamageable>().TakeDamage(projectileDamage);
+                }
+            }
+
+            timeSinceLastShot = 0f;
         }
     }
 }
--- FPS/Assets/_Project/Scripts/SimpleEnemy.cs
+++ FPS/Assets/_Project/Scripts/SimpleEnemy.cs
@@ -7,17 +7,40 @@ public class SimpleEnemy : MonoBehaviour, IDamageable
 {
     public Transform player;
     public float health;
-    private NavMeshAgent agent;
+    public AudioClip gotHitSound;
+    public AudioClip deathSound;
 
+    private NavMeshAgent agent;
     private Rigidbody rb;
+    private Animator animator;
+    private AudioSource audio;
+
+    private bool isDead = false;
 
     public void TakeDamage(float damage)
     {
         health -= damage;
 
+        if (audio && gotHitSound)
+            audio.PlayOneShot(gotHitSound);
+
         if(health <= 0f)
         {
-            Destroy(gameObject);
+            if(animator)
+                animator.SetTrigger("Dying");
+
+            if (audio && deathSound)
+                audio.PlayOneShot(deathSound);
+
+            isDead = true;
+            agent.enabled = false;
+
+            Collider[] colliders = GetComponentsInChildren<Collider>();
+            foreach (var col in colliders)
+            {
+                col.enabled = false;
+            }
+            this.enabled = false;
         }
     }
 
@@ -27,11 +50,21 @@ public class SimpleEnemy : MonoBehaviour, IDamageable
         agent = GetComponent<NavMeshAgent>();
         rb = GetComponent<Rigidbody>();
         rb.isKinematic = true;
+        animator = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+
+        animator.SetFloat("WalkOffset", Random.Range(0f, 1f));
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isDead)
+            return;
+
         agent.SetDestination(player.position);
+
+        if(animator)
+            animator.SetFloat("Speed", agent.velocity.magnitude);
     }
 }
